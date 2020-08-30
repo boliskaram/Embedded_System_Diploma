bca0855a7bc42f52f4979eb636bbec30a85ecd49
Bolis Karam <boliskaram8@gmail.com>
2020-08-30 20:01:12 +0200

    C Program to Swap Two Numbers without temp variable

---
+++ Programming C/Assignment 1 C-Basic/EX7 C Program to Swap Two Numbers without temp variable/src/Swap Two Numbers without temp variable.c
@@ -0,0 +1,27 @@
+/*
+ ============================================================================
+ Name        : Assignment.c
+ Author      : Bolis Karam
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	float a, b ;
+		printf("Enter value of a: ");
+		fflush(stdout);
+		scanf("%f",&a);
+		printf("Enter value of b: ");
+		fflush(stdout);
+		scanf("%f",&b);
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		printf("After swapping, value of a = %f\n",a);
+		printf("After swapping, value of b = %f",b);
+}
