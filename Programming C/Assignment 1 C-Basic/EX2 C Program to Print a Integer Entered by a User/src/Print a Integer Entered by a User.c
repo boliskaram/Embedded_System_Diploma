de05d2ad09a7e88bcd9b086f69f0bccc3410a403
Bolis Karam <boliskaram8@gmail.com>
2020-08-30 19:58:34 +0200

    C Program to Print a Integer Entered by a User

---
+++ Programming C/Assignment 1 C-Basic/EX2 C Program to Print a Integer Entered by a User/src/Print a Integer Entered by a User.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : Assignment.c
+ Author      : Bolis Karam
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int x;
+	printf("Enter a integer: ");
+	fflush(stdout);
+	scanf("%d",&x);
+	printf("You entered: %d",x);
+}
