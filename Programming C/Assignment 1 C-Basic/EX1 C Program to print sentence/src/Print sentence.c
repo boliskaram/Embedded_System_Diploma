e32d287f6495e65313d1d36c692bf788bbd07722
Bolis Karam <boliskaram8@gmail.com>
2020-08-30 19:56:52 +0200

    C program to print sentence

---
+++ Programming C/Assignment 1 C-Basic/EX1 C Program to print sentence/src/Print sentence.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : Assignment.c
+ Author      : Bolis Karam
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("C Programming");
+}
